34e149dcecfbfa0eca0cb9108711ddfb7d5ffd1a
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-09 17:40:12 -0300

    Update settings.py

--- backend/chems_tools/settings.py
+++ backend/chems_tools/settings.py
@@ -38,11 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # 3rd Party Apps
+    'rest_framework',
+    'corsheaders',
+
+    # Local Apps
+    'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # middleware de CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # Permitir peticiones desde frontend
+    "http://127.0.0.1:3000",
+]
