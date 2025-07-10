b7642e9fb3593118c18f727980d4ee3f287be7fb
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-09 23:50:23 -0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+# This view allows users to retrieve their own profile information.
+class UserProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
