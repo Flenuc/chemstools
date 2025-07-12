9f0fe3dfec0fb36e7afafacaeebc77524be527e9
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-12 13:11:47 -0300

    Update index.ts

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import moleculesReducer from './moleculesSlice';
+import notificationsReducer from './notificationsSlice';
+import filterReducer from './filterSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     molecules: moleculesReducer,
+    notifications: notificationsReducer,
+    filters: filterReducer, 
   },
 });
 
