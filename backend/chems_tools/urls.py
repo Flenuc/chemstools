099c2e10bf25506b5926da8b3513b2e7b372fa3a
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-11 23:03:03 -0300

    Update urls.py

--- backend/chems_tools/urls.py
+++ backend/chems_tools/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/molecules/', include('molecules.urls')),
+    path('api/data/', include('data.urls')),
 ]
