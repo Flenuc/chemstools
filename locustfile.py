57974c5b455b96002bff72de7cf4a5bd4ab21bc7
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-27 20:07:41 -0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -9,13 +9,13 @@ class ChemsToolsUser(HttpUser):
         self.username = f"user_{self.environment.runner.user_count}"
         self.password = "password123"
 
-        self.client.post("/api/auth/register/", json={
+        self.client.post("api/auth/register/", json={
             "username": self.username,
             "password": self.password,
             "email": f"{self.username}@example.com"
         })
 
-        res = self.client.post("/api/auth/token/", json={
+        res = self.client.post("api/auth/token/", json={
             "username": self.username,
             "password": self.password
         })
@@ -25,11 +25,11 @@ class ChemsToolsUser(HttpUser):
 
     @task(5) # Esta tarea se ejecutará 5 veces más que las otras
     def get_molecules(self):
-        self.client.get("/api/molecules/")
+        self.client.get("api/molecules/")
 
     @task
     def create_molecule(self):
-        self.client.post("/api/molecules/", json={
+        self.client.post("api/molecules/", json={
             "name": "Caffeine",
             "structure_data": "CN1C=NC2=C1C(=O)N(C(=O)N2C)C",
             "format": "SMILES"
