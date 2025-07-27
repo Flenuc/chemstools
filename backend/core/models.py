4f829639becaad7f974e1010a910f9bc65fc6311
Flenuc <fernan.caprino.et28@gmail.com>
2025-07-27 20:07:10 -0300

    Update models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    """
+    Un modelo de clase base abstracto que proporciona campos
+    ``created_at`` y ``updated_at`` que se actualizan automáticamente.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
