e3c856ac011bc7a91db75eb11c5c1ad053b329d0
Flenuc <fernan.caprino.et28@gmail.com>
2025-08-10 03:15:00 -0300

    Update urls.py

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuizViewSet
+from .views import QuizViewSet, ChemWordleViewSet
 
 router = DefaultRouter()
 router.register(r'quiz', QuizViewSet, basename='quiz')
+router.register(r'chemwordle', ChemWordleViewSet, basename='chemwordle')
 
 urlpatterns = [
     path('', include(router.urls)),
